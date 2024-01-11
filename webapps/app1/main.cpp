44501588db401ff01a58fe4cbf7bad151a24c383
wu <2461519090@qq.com>
2024-01-11 19:33:34 +0800

    修改

--- webapps/app1/main.cpp
+++ webapps/app1/main.cpp
@@ -3,13 +3,14 @@
 #include <filesystem>
 #include <unistd.h>
 
-#include "base_class.h"
+#include "../../base_class.h"
+
 
 class Root: public BaseClass {
 public:
     Root();
-    void get(Request *request, Response *response, const std::string &cur_path) override;
-    void post(Request *request, Response *response, const std::string &cur_path) override;
+    std::string doGet(Request *request, Response *response, const std::string &cur_path) override;
+    void doPost(Request *request, Response *response, const std::string &cur_path) override;
     ~Root() override;
 private:
 };
@@ -17,23 +18,37 @@ private:
 Root::Root() {
 }
 
-void Root::get(Request *request, Response *response, const std::string &cur_path) {
+std::string Root::doGet(Request *request, Response *response, const std::string &cur_path) {
     Session session = request->getSession();
-    auto m = session.getAttributes();
-    std::string s = "当前有" + std::to_string(m.size()) + "个属性";
-    for (auto it = m.cbegin(); it != m.cend(); it++) {
-        s.append("\n").append(it->first).append(": ").append(it->second);
+    std::string s;
+    if (session) {
+        auto m = session.getAttributes();
+        s = "当前有" + std::to_string(m.size()) + "个属性";
+        for (auto it = m.cbegin(); it != m.cend(); it++) {
+            s.append("\n").append(it->first).append(": ").append(it->second);
+        }
+    } else {
+        s = "cookie错误";
     }
     response->write_data(s);
+    return {};
 }
 
-void Root::post(Request *request, Response *response, const std::string &cur_path) {
+void Root::doPost(Request *request, Response *response, const std::string &cur_path) {
     auto m = request->getParams();
     Session session = request->getSession();
+    int add = 0, set = 0;
     for (auto it = m.cbegin(); it != m.cend(); it++) {
-        session.setAttribute(it->first, it->second);
+        if (session.addAttribute(it->first, it->second)) {
+            ++add;
+        } else {
+            set += session.setAttribute(it->first, it->second);
+        }
     }
-    std::string s = "添加了" + std::to_string(m.size()) + "个属性";
+    std::string s = "添加了" + std::to_string(add) + "个属性";
+    if (set != 0) {
+        s.append("，修改了").append(std::to_string(set)).append("个属性");
+    }
     response->write_data(s);
 }
 
